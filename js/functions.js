a8af2f483bfed2451e72b4ad7c67deeb039165cd
Teddy Raven <teddyraven@Teddys-MBP.hsd1.ca.comcast.net>
2018-09-19 12:49:20 -0700

    Answered all questions, left some test script deactivated just so you can see all of my mistakes. This was challenging! Especially the labelOrder fuction, but after that similar rules applied so things made more sense.

--- js/functions.js
+++ js/functions.js
@@ -1,22 +1,47 @@
 
-function sayHello(name) {
+function sayHello() {
+document.write('Hello');
   // TODO: return a greeting string that greets
   // the person by name, e.g. "Hello, Emily!"
 }
+function myName() {
+  document.write(' Teddy!');
+}
 
 function sayHelloEverybody(threeNames) {
+  var threeNames = [' Teddy',' Emily',' and Santa Claus!'];
+  document.write(threeNames)
+
+
   // TODO: return a greeting string that greets
   // all three people in the threeNames array
   // e.g. "Hello, Emily, MaryClare, and Taurin!"
   // NOTE: inputs should be an array with exactly three items
 }
 
-function getClasses() {
+function getClasses(classes) {
+  var classes = ['Music Business',' Private lessons with Clint',
+'Tools, techniques, and analysis',' Intro to Computer Science',' Logic class',
+' Intro to Recording',' Sound Design',' and the Sony Project']
+  document.write(classes)
   // TODO: return an array with a list of strings
   // naming the classes you are taking this semester
 }
 
 function labelOrder(fiveNumbers) {
+  var fiveNumbers = [1,2,3,4,5];
+var newArray = ['','','','',''];
+
+newArray[0] = ' first:' + fiveNumbers[0];
+newArray[1] = ' second:' + fiveNumbers[1];
+newArray[2] = ' third:' + fiveNumbers[2];
+newArray[3] = ' fourth:' + fiveNumbers[3];
+newArray[4] = ' fifth:' + fiveNumbers[4];
+
+
+return(newArray);
+
+
   // TODO: Given an array with five numbers, return a new array
   // of strings that have labels for the ordering.
   // e.g. if fiveNumbers is [1,2,3,4,5] your function should return:
@@ -27,9 +52,23 @@ function labelOrder(fiveNumbers) {
 function calculateVolume(w, l, h) {
   // TODO: given a room's width, length, and height
   // write a function that returns the volume
+var volume = w * l * h;
+return volume;
+
+
+
+
 }
 
 function isOdd(n) {
+return n % 2 == 1;
+
+//
+//var Odd = n % 2 == 1;
+//Odd = Boolean(1)
+//var Even = n % 2 == 0;
+//Even = Boolean(0)
+//return n;//
   // TODO: given a number, return true if it is odd, false if it is even
   // HINT: the expression "Boolean(0)" is false, "Boolean(1)"" is true
   // DOUBLE HINT: Try using the modulus operator (%)
@@ -39,6 +78,15 @@ function isOdd(n) {
 
 const saleAmount = .25 // 25% off!
 function getSalePrice(originalPrice) {
+//var salePrice = number * saleAmount;
+
+return originalPrice - (saleAmount * originalPrice);
+
+//  var price = number;
+//  var salePrice = price * saleAmount;
+//return(salePrice);
+
+
   // TODO: given a dollar price (e.g. 5.99), return
   // a price with the saleAmount discount applied
   // e.g. if originalPrice is 4.00, your function should return 3.00.
@@ -46,6 +94,16 @@ function getSalePrice(originalPrice) {
 }
 
 function getSalePrices(threeOriginalPrices) {
+
+var newArray = ['','',''];
+
+newArray[0] = getSalePrice(3);
+newArray[1] = getSalePrice(4);
+newArray[2] = getSalePrice(5);
+
+return(newArray);
+
+
   // TODO: given an array of prices for three items in a cart,
   // return a new array with the saleAmount discount applied to each.
   // TIP: see if you can reuse the getSalePrice function that you just wrote!
@@ -54,8 +112,10 @@ function getSalePrices(threeOriginalPrices) {
 
  // ** COUNTING ** //
 
-var counter = 0;
+var counter = 4;
 function incrementCounter() {
+  var newNumber = counter + 1;
+  return(newNumber);
   // TODO: implement a function that increments (adds 1) to the counter variable
   // and then calls showCounter to update the interface with the new value.
   // Test your code by opening the developer console and calling incrementCounter.
